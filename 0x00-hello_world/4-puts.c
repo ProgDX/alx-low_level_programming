d6c4f696968884cfdc8d1e39acffcd52177d18f3
Hassan Ssembatya <hassanssembatya3@gmail.com>
2023-07-14 23:28:33 -0700

    puts in action

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ *
+ *Use of puts in C
+ *main - Entry point
+ */
+int main (){
+  char str[50];
+
+  strcpy(str1,"Programming is like building a multilingual puzzle");
+
+  puts(str1);
+
+  return (0);
+}
