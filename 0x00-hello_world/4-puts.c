661622da22a5bb2f9aad50365c3473022842c81a
Hassan Ssembatya <hassanssembatya3@gmail.com>
2023-07-15 01:28:26 -0700

    tried

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,13 +6,10 @@
  *Use of puts in C
  *main - Entry point
  */
-int main (void)
+int main(void)
 {
 char str[52];
 
- strcpy(str,"\"Programming is like building a multilingual puzzle");
-
- puts(str);
-
- return (0);
-}
+strcpy(str,"\"Programming is like building a multilingual puzzle");
+puts(str);
+ return (0);}
