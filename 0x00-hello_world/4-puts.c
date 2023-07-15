46b33a9514387c9b413446bf8e202e96ee0d88b3
Hassan Ssembatya <hassanssembatya3@gmail.com>
2023-07-15 01:44:46 -0700

    hy

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include<stdio.h>
 #include<string.h>
-
 /**
- *Use of puts in C
- *main - Entry point
- */
+* main - print the string in the put function
+*
+* Description: using the main function
+* this program prints "Programming is like building a multilingual puzzle
+* Return: 0
+*/
 int main(void) /*if function ends return 0*/
 {
 char str[52];
