6ec6fc04372c5e1fcfc6627c41db02d866e52157
Hassan Ssembatya <hassanssembatya3@gmail.com>
2023-07-15 02:14:07 -0700

    none

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,9 +2,9 @@
 /**
 *main- prints the result of the printf
 *function prints the exact value
-*return 0 for null
+*return: 0
 */
-int main(void) /*return 0*/
+int main(void) /*return: 0*/
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
