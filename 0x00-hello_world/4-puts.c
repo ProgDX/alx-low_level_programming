f16106e4cfd9593d201fcfa606a94e1f60149779
Hassan Ssembatya <hassanssembatya3@gmail.com>
2023-07-15 01:31:23 -0700

    how about that

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -10,6 +10,7 @@ int main(void)
 {
 char str[52];
 
-strcpy(str,"\"Programming is like building a multilingual puzzle");
+strcpy(str, "\"Programming is like building a multilingual puzzle");
 puts(str);
+ 
  return (0);}
