ce4088c48acb4b22f5012a5864a56c3f15e0a13b
Hassan Ssembatya <hassanssembatya3@gmail.com>
2023-07-17 14:30:01 -0700

    trying my best

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - print the string in the put function
+*
+* Description: using the main function
+* this program prints "Programming is like building a multilingual puzzle
+* Return: 0
+*/
+int main()
+{
+for(int x = 'A'; x <= 'Z'; x++) // jumps from 'A'(65) to 'B'(66) to ... in order.
+{
+char small = tolower(x);
+putchar(small);
+}
+return 0;
+}
