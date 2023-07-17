d9fbec4b33e99f927418525f4b43b0ca610cc28b
Hassan Ssembatya <113818559+ProgDX@users.noreply.github.com>
2023-07-17 15:17:02 +0300

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,15 +15,15 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
-printf("%d is %s\n",n, "positive");
+printf("%d is %s\n", n, "positive");
 }
-else if (n==0)
+else if (n == 0)
 {
-printf("%d is %s\n",n, "zero");
+printf("%d is %s\n", n, "zero");
 }
 else
 {
-printf("%d is %s\n",n, "negative");
+printf("%d is %s\n", n, "negative");
 }
 return (0);
 }
