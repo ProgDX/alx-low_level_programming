3b89a2d636c9cdbd43ba4caebdfee33c60a4c9e5
Hassan Ssembatya <hassanssembatya3@gmail.com>
2023-07-18 14:41:53 -0700

    tried

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+* main - print the string in the put function
+*
+* Description: using the main function
+* this program printsProgramming is like building a multilingual puzzle
+* Return: 0
+*/
+int main(void)
+{
+int i;
+for (i = 0; i <= 9; i++);
+putchar(i);
+if (i != 9)
+{
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
