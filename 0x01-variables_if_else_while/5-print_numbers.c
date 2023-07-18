fcb41e53e607b1bf538eb2d30b8e12970fe029e7
Hassan Ssembatya <hassanssembatya3@gmail.com>
2023-07-18 13:46:06 -0700

    hi

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+* main - print the string in the put function
+*
+* Description: using the main function
+* this program printsProgramming is like building a multilingual puzzle
+* Return: 0
+*/
+int main(void)
+{
+int a;
+while (a < 10)
+{
+printf("%d\n", a);
+}
+return (0);
+}
