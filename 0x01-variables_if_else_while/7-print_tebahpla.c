61899d0c034084082a5cffc67fd45b7eeef0e5a2
Hassan Ssembatya <hassanssembatya3@gmail.com>
2023-07-18 14:20:57 -0700

    backwards

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+* main - print the string in the put function
+*
+* Description: using the main function
+* this program printsProgramming is like building a multilingual puzzle
+* Return: 0
+*/
+int main(void)
+{
+char num;
+for (num = 'z'; num >= 'a'; num--)
+putchar(num);
+putchar('\n');
+return (0);
+}
