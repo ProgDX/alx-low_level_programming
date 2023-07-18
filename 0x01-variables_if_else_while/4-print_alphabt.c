9caccd479db53c8b3f703d9e6d26172d72175ba9
Hassan Ssembatya <hassanssembatya3@gmail.com>
2023-07-18 13:30:44 -0700

    wrong dir

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**                                                                  
+* main - print the string in the put function                        *                                                                    
+* Description: using the main function                               
+* this program printsProgramming is like building a multilingual puzz\
+le                                                                   * Return: 0                                                          
+*/
+int main(void)
+{
+int lowerCase = 'a';
+while (lowerCase <= 'z')
+{
+if (lowerCase == 'e' || lowerCase == 'q')
+{
+lowerCase += 1;
+}
+else
+{
+putchar(lowerCase);
+lowerCase += 1;
+}
+}
+putchar('\n');
+return (0);
+}
